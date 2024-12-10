60161c14a7cba2258641dcf69fbec94a5fe804e9
joynalabed5 <engr.joynal.bd@gmail.com>
2024-12-10 23:24:28 +0600

    variadic_2

--- functions.php
+++ functions.php
@@ -71,3 +71,22 @@ function sumMyNumbers(...$x) {
 
 ?>
 
+
+//The variadic argument must be the last argument
+
+<?php
+function myFamily($lastname, ...$firstname) {
+  $txt = "";
+  $len = count($firstname);
+  for($i = 0; $i < $len; $i++) {
+    $txt = $txt."Hi, $firstname[$i] $lastname.<br>";
+  }
+  return $txt;
+}
+
+$a = myFamily("Doe", "Jane", "John", "Joey");
+echo $a;
+?>
+
+
+
